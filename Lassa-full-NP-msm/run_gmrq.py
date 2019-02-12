a9fd1330e891cbafcef1afe659062cd02ab53a91
Jason Pattis <jason.pattis@uconn.edu>
2019-02-12 11:05:13 -0500

    changed spacing in gmrq script

--- Lassa-full-NP-msm/run_gmrq.py
+++ Lassa-full-NP-msm/run_gmrq.py
@@ -26,38 +26,38 @@ print(len(ttrajs))
 results = []
 
 for lag in lags:
-	for n_clusters in cl:
-		for comp in components:
-			model.set_params(cluster__n_clusters=n_clusters, tica__lag_time=lag, tica__n_components=comp)
-			for fold, (train, validate) in enumerate(shuffle_split):
-				try:
-					train_ttrajs = [ ftrajs_list[i] for i in train ]
-					val_ttrajs = [ ftrajs_list[i] for i in validate ]
+    for n_clusters in cl:
+        for comp in components:
+            model.set_params(cluster__n_clusters=n_clusters, tica__lag_time=lag, tica__n_components=comp)
+                for fold, (train, validate) in enumerate(shuffle_split):
+                    try:
+                        train_ttrajs = [ ftrajs_list[i] for i in train ]
+                        val_ttrajs = [ ftrajs_list[i] for i in validate ]
 
-					model.fit(train_ttrajs)
+                        model.fit(train_ttrajs)
 
-					train_score = model.score(train_ttrajs)
-					test_score = model.score(val_ttrajs)
+                        train_score = model.score(train_ttrajs)
+                        test_score = model.score(val_ttrajs)
 
-					msm_timescales = model.named_steps['msm'].timescales_
-					pr = model.named_steps['msm'].percent_retained_
-					print('I did something ', fold)
-					print('clusters lag components train test', n_clusters, lag, comp, train_score, test_score)
-					results.append({
-						'train_score': train_score,
-						'test_score': test_score,
-						'n_clusters': n_clusters,
-						'components': comp,
-						'msm_timescale0': msm_timescales[0],
-						'msm_timescale1': msm_timescales[1],
-						'percent_retained' : pr,
-						'tica_lag': lag,
-						'fold': fold
-					})
+                        msm_timescales = model.named_steps['msm'].timescales_
+                        pr = model.named_steps['msm'].percent_retained_
+                        print('I did something ', fold)
+                        print('clusters lag components train test', n_clusters, lag, comp, train_score, test_score)
+                        results.append({
+                            'train_score': train_score,
+                            'test_score': test_score,
+                            'n_clusters': n_clusters,
+                            'components': comp,
+                            'msm_timescale0': msm_timescales[0],
+                            'msm_timescale1': msm_timescales[1],
+                            'percent_retained' : pr,
+                            'tica_lag': lag,
+                            'fold': fold
+                         })
 
-				except:
-					print('something went wrong! -Jason')
-					pass
+                     except:
+                         print('something went wrong! -Jason')
+                         pass
 
 results1 = pd.DataFrame(results)
 
